af234986713d69cf3c55238384b14ac5a4f030a3
IT1-2021 <IT1-2021@AR4-02.mshome.net>
2022-03-14 09:19:49 +0100

    Druge vezbe

--- Inicijalni_projekat/src/geometry/Test.java
+++ Inicijalni_projekat/src/geometry/Test.java
@@ -1,5 +1,5 @@
 package geometry;
 
 public class Test {
-
+	
 }
--- Inicijalni_projekat/src/introduction/HelloWorld.java
+++ Inicijalni_projekat/src/introduction/HelloWorld.java
@@ -3,8 +3,10 @@ package introduction;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, World!");
+		
+			
+		
+		
 
 	}
 
